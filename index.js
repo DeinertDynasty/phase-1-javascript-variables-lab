db28288ad9a54cda10dc16dcf662777be8976df8
Phoenix Deinert <phoenix.deinert@gmail.com>
2023-03-15 01:30:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+
+
+// 2) mostProfitableNeighborhood is declared as equal to Chelsea
+// 3) mostProfitableNeighborhood is defined using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// 4) companyCeo is declared as equal to Susan Smith
+// 5) companyCeo is defined using let
+let companyCeo = 'Susan Smith';
